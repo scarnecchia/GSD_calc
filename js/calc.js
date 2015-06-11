9ef8e9fae618eb1e012130399a08811380d8066f
scarnecchia <scarnecchia@gmail.com>
2015-06-11 16:10:14 -0400

    GSD .02
    Now does Altitude.

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,6 @@
-$( "#area_info" ).tooltip({
-  position: { my: "left+15 center", at: "right center" }
-});
+/*pixel size Calculations*/
 
-
-function calc_pix_size() {
+function calc_pix_size(){
     var sensor_width = document.getElementById("sensor_width").value;
     var sensor_height = document.getElementById("sensor_height").value;
     var image_width = document.getElementById("image_width").value;
@@ -12,17 +9,31 @@ function calc_pix_size() {
     var px_size = Math.sqrt((sensor_width * sensor_height) / (image_width * image_height));
 
     var pixel_size = math.round(px_size, 6);
-
-    document.getElementById("pixel_size").value = pixel_size;
     document.getElementById("px").value = pixel_size;
+    document.getElementById("px2").value = pixel_size;
 }
 
-var button = document.getElementById("button");
-button.onclick = calc_pix_size;
+var GSD_button = document.getElementById("GSD_select");
+GSD_button.onclick = calc_pix_size;
 
-function calc_gsd() {
+var AGL_select = document.getElementById("AGL_select");
+AGL_select.onclick = calc_pix_size;
+
+$( "#GSD_select" ).click(function() {
+  $( "#AGL_column" ).hide("fast");
+  $( "#GSD_column" ).show("fast");
+});
+
+$( "#AGL_select" ).click(function() {
+  $( "#GSD_column" ).hide("fast");
+  $( "#AGL_column" ).show("fast");
+});
+
+/*GSD Calculations*/
+
+function calc_gsd(){
   var px = document.getElementById("px").value;
-  var altitude = document.getElementById("altitude").value;
+  var altitude = document.getElementById("AGL_known").value;
   var flength = document.getElementById("focal_length").value;
 
   var gsdur = (px * altitude / flength) * 100;
@@ -50,3 +61,23 @@ function todo() {
 
 var gsd_button = document.getElementById("gsd_button");
 gsd_button.onclick = todo;
+
+$( "#area_info" ).tooltip({
+  position: { my: "left+15 center", at: "right center" }
+});
+
+/*Altitude Calculations*/
+
+function calc_agl(){
+  var px = document.getElementById("px2").value;
+  var gsd = document.getElementById("GSD_known").value;
+  var flength = document.getElementById("focal_length2").value;
+
+  var aglur = (gsd * flength / px) / 100;
+  var agl = math.round(aglur, 2);
+
+  document.getElementById("agl").value = agl;
+}
+
+var AGL_button = document.getElementById("AGL_button");
+AGL_button.onclick = calc_agl;
